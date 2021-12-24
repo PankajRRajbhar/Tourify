99202695c2d66d7ede912d24af70939bf6c35560
utopia <pankajraj1278@gmail.com>
2021-12-24 08:11:29 +0530

    Initial Commit

---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,16 @@
+<meta name="description" content="">
+<meta name="author" content="">
+
+        <title>Startmin - Bootstrap Admin Theme</title>
+
+        <!-- Bootstrap Core CSS -->
+        <link href="../css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- MetisMenu CSS -->
+        <link href="../css/metisMenu.min.css" rel="stylesheet">
+
+        <!-- Custom CSS -->
+        <link href="../css/startmin.css" rel="stylesheet">
+
+        <!-- Custom Fonts -->
+        <link href="../css/font-awesome.min.css" rel="stylesheet" type="text/css">
---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @include('includes.head');
+    <title>@yield('title')</title>
+</head>
+<body>
+    <div id="wrapper">
+
+            @include('includes.header')
+            @include('includes.sidenav')
+
+            <!-- Page Content -->
+        <div id="page-wrapper">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        @yield('content')
+                    </div>
+                        <!-- /.col-lg-12 -->
+                </div>
+                    <!-- /.row -->
+            </div>
+                <!-- /.container-fluid -->
+        </div>
+
+        </div>
+    </div>
+
+    @include('includes.scripts')    
+
+</body>
+</html>
